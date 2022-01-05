ae3f0d15c1f1ae21e6cb2a0f56f5ba2b648227fd
stiven212 <stivfunk2000@hotmail.com>
2022-01-05 18:01:46 -0500

    changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { CallbackID, Capacitor } from '@capacitor/core';
+import { Geolocation } from '@capacitor/geolocation';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,52 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  coordinate: any;
+  watchCoordinate: any;
+  watchId: any;
 
-  constructor() {}
+  constructor(private zone: NgZone) { }
 
+  async requestPermissions() {
+    const permResult = await Geolocation.requestPermissions();
+    console.log('Perm request result: ', permResult);
+  }
+
+  getCurrentCoordinate() {
+    if (!Capacitor.isPluginAvailable('Geolocation')) {
+      console.log('Plugin geolocation not available');
+      return;
+    }
+    Geolocation.getCurrentPosition().then(data => {
+      this.coordinate = {
+        latitude: data.coords.latitude,
+        longitude: data.coords.longitude,
+        accuracy: data.coords.accuracy
+      };
+    }).catch(err => {
+      console.error(err);
+    });
+  }
+
+  watchPosition() {
+    try {
+      this.watchId = Geolocation.watchPosition({}, (position, err) => {
+        console.log('Watch', position);
+        this.zone.run(() => {
+          this.watchCoordinate = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          };
+        });
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  clearWatch() {
+    if (this.watchId != null) {
+      Geolocation.clearWatch({ id: this.watchId });
+    }
+  }
 }
